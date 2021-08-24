cd19532112736d994dc8a66c86f5984137ff07c0
MihajaUser <81333217+MihajaUser@users.noreply.github.com>
2021-08-25 00:00:33 +0300

    test1

--- application/models/Categories.php
+++ application/models/Categories.php
@@ -13,14 +13,4 @@ class Categories extends CI_Model
         }
         return $query->result_array();
     }
-    public  function getCategories6()
-    {
-        $row = array();
-        $query = $this->db->query('select * from Categories ');
-        foreach ($query->result_array() as $row) {
-            $row['id'];
-            $row['nom'];
-        }
-        return $query->result_array();
-    }
 }
